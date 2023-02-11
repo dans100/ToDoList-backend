9e3b41e8d4842b55aba5a6d0784fa3dc41923598
Daniel <111749405+dans100@users.noreply.github.com>
2023-02-11 13:26:01 +0100

    Delete db.ts

--- utils/db.ts
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'todo_list',
-    decimalNumbers: true,
-    namedPlaceholders: true,
-});
