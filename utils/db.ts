f21c343c5adf529008ae918ecbfce6d5fe2beda6
Daniel Sulkowski <dansulk@gmail.com>
2023-02-14 12:04:59 +0100

    Added import mysql

--- utils/db.ts
+++ utils/db.ts
@@ -4,6 +4,7 @@ export const pool = mysql.createPool({
     host: process.env.DBHOST as string,
     user: process.env.DBUSER as string,
     database: process.env.DATABASE as string,
+	password: process.env.DBPASSWORD as string,
     decimalNumbers: true,
     namedPlaceholders: true,
 });
