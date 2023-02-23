4d4be4ff227b5ba8a3c46e26344744e12899f8f3
Daniel Sulkowski <dansulk@gmail.com>
2023-02-23 21:41:30 +0100

    Modified database and relations added

--- routes/list.ts
+++ routes/list.ts
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import {Request, Response, Router} from "express";
 import {TaskRecord} from "../records/taskRecord";
 import {ValidationError} from "../utils/error";
 import {verifyToken} from "../utils/verifyToken";
@@ -7,16 +7,21 @@ import {verifyToken} from "../utils/verifyToken";
 export const list = Router();
 
 list
-    .get('/:name?', verifyToken, async (req, res) => {
-        const list = await TaskRecord.listAll(req.params.name ?? '');
+    .get('/:name?', verifyToken, async (req: Request, res: Response) => {
+
+        const list = await TaskRecord.listAll(req.user.id, req.params.name ?? '');
         res.json(list);
     })
-    .post('/', verifyToken, async (req, res) => {
-        const task = new TaskRecord(req.body);
+    .post('/', verifyToken, async (req: Request, res: Response) => {
+
+        const task = new TaskRecord({
+            ...req.body,
+            user_id: req.user.id,
+        });
         await task.insert();
         res.json(task);
     })
-    .delete('/:id?', verifyToken, async(req, res) => {
+    .delete('/:id?', verifyToken, async (req: Request, res: Response) => {
         if (req.params.id) {
             const task = await TaskRecord.getOne(req.params.id);
             if (task === null) {
@@ -25,19 +30,19 @@ list
             await task.delete();
             res.json(task);
         } else {
-            await TaskRecord.deleteAll();
+            await TaskRecord.deleteAll(req.user.id);
             res.end();
         }
     })
-    .patch('/:id', verifyToken, async(req, res) => {
+    .patch('/:id', verifyToken, async (req, res) => {
         const task = await TaskRecord.getOne(req.params.id);
         if (task === null) {
             return new ValidationError('Task not found')
         }
-        if (req.body.editTaskValue.length <3 || req.body.editTaskValue.length > 55) {
+        if (req.body.editTaskValue.length < 3 || req.body.editTaskValue.length > 55) {
             return new ValidationError('Task cannot be more than 3 characters and later than 55 characters');
         }
-        task.task = req.body.editTaskValue;
+        task.description = req.body.editTaskValue;
         await task.update();
         res.json(task);
     });
--- utils/db.ts
+++ utils/db.ts
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise';
+
 export const pool = mysql.createPool({
     host: process.env.DBHOST as string,
     user: process.env.DBUSER as string,
     database: process.env.DATABASE as string,
-	password: process.env.DBPASSWORD as string,
+    password: process.env.DBPASSWORD as string,
     decimalNumbers: true,
     namedPlaceholders: true,
 });
