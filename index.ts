a4f5d1ca59fb97917589d1a3ae534b441416b3cd
Daniel Sulkowski <dansulk@gmail.com>
2023-03-07 20:54:02 +0100

    formated code withe prettier

--- index.ts
+++ index.ts
@@ -3,47 +3,48 @@ require('dotenv').config();
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import rateLimit from "express-rate-limit";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import {list} from "./routes/list";
-import {register} from "./routes/register";
-import {login} from "./routes/login";
+import rateLimit from 'express-rate-limit';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { list } from './routes/list';
+import { register } from './routes/register';
+import { login } from './routes/login';
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 60 * 1000,
+  max: 500,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 const app = express();
 app.use(limiter);
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN as string,
     methods: 'GET,POST,DELETE,PATCH',
     credentials: true,
-}));
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
     secret: process.env.SESSION_TOKEN as string,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 60 * 60 * 24,
-    }
-}))
-
+      maxAge: 60 * 60 * 24,
+    },
+  })
+);
 
 app.use('/list', list);
 app.use('/register', register);
 app.use('/', login);
 
-
 app.listen(3001, '0.0.0.0', () => {
-    console.log('Server listen on port: http://0.0.0.0:3001');
+  console.log('Server listen on port: http://0.0.0.0:3001');
 });
-
--- utils/error.ts
+++ utils/error.ts
@@ -1,15 +1,17 @@
-import {ErrorRequestHandler, Request, Response} from "express";
+import { ErrorRequestHandler, Request, Response } from 'express';
 
-export class ValidationError extends Error {
-}
+export class ValidationError extends Error {}
 
-export const handleError = (err: ErrorRequestHandler, req: Request, res: Response) => {
+export const handleError = (
+  err: ErrorRequestHandler,
+  req: Request,
+  res: Response
+) => {
+  console.error(err);
 
-    console.error(err);
-
-    res
-        .status(err instanceof ValidationError ? 400 : 500)
-        .json(err instanceof ValidationError ? err.message : 'Spróbuj ponownie później');
+  res
+    .status(err instanceof ValidationError ? 400 : 500)
+    .json(
+      err instanceof ValidationError ? err.message : 'Spróbuj ponownie później'
+    );
 };
-
-
